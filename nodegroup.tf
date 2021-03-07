49da56d67196263458cd06dcc8016e9ec8d50518
root <root@ip-172-31-31-149.eu-west-1.compute.internal>
2021-03-07 17:33:22 +0000

    first

---
+++ nodegroup.tf
@@ -0,0 +1,57 @@
+#
+# EKS Worker Nodes Resources
+#  * IAM role allowing Kubernetes actions to access other AWS services
+#  * EKS Node Group to launch worker nodes
+#
+
+resource "aws_iam_role" "seciln" {
+  name = "terraform-eks-seciln"
+
+  assume_role_policy = <<POLICY
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Effect": "Allow",
+      "Principal": {
+        "Service": "ec2.amazonaws.com"
+      },
+      "Action": "sts:AssumeRole"
+    }
+  ]
+}
+POLICY
+}
+
+resource "aws_iam_role_policy_attachment" "seciln-AmazonEKSWorkerNodePolicy" {
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSWorkerNodePolicy"
+  role       = aws_iam_role.seciln.name
+}
+
+resource "aws_iam_role_policy_attachment" "seciln-AmazonEKS_CNI_Policy" {
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKS_CNI_Policy"
+  role       = aws_iam_role.seciln.name
+}
+
+resource "aws_iam_role_policy_attachment" "seciln-AmazonEC2ContainerRegistryReadOnly" {
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEC2ContainerRegistryReadOnly"
+  role       = aws_iam_role.seciln.name
+}
+
+resource "aws_eks_node_group" "demo" {
+  cluster_name    = aws_eks_cluster.demo.name
+  node_group_name = "demo"
+  node_role_arn   = aws_iam_role.seciln.arn
+  subnet_ids      = ["subnet-d082adsfsd","subnet-5efxxx"]
+  scaling_config {
+    desired_size = 1
+    max_size     = 1
+    min_size     = 1
+  }
+
+  depends_on = [
+    aws_iam_role_policy_attachment.seciln-AmazonEKSWorkerNodePolicy,
+    aws_iam_role_policy_attachment.seciln-AmazonEKS_CNI_Policy,
+    aws_iam_role_policy_attachment.seciln-AmazonEC2ContainerRegistryReadOnly,
+  ]
+}
